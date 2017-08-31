1f3674d54ceab5ac3e5ecf1689e2a5211cfb4416
jamsya <jamsya@naver.com>
2017-09-01 01:27:11 +0900

    메모 추가 모드에서 사진넣기 성공. 이제 인텐트를 통해 옮겨주면 된다.

--- app/src/main/java/nova/workoutapp22/AddMemoActivity.java
+++ app/src/main/java/nova/workoutapp22/AddMemoActivity.java
@@ -1,25 +1,47 @@
 package nova.workoutapp22;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static nova.workoutapp22.MotivationActivity.CROP_FROM_IMAGE;
+import static nova.workoutapp22.MotivationActivity.PICK_FROM_ALBUM;
+import static nova.workoutapp22.MotivationActivity.PICK_FROM_CAMERA;
 import static nova.workoutapp22.timeController.getTimeCutSec;
 
 public class AddMemoActivity extends AppCompatActivity {
 
+    private Uri mImageCaptureUri;
+    private ImageView iv_User_Photo;
+    private String absolutePath;
+
+
+
     EditText editTextMemo;
     TextView strDate;
-    ImageView photo;
+    ImageView imageView;
+    LinearLayout imageFrame;
     int mIDForTransport;
 
     String strMemoMode;
@@ -31,9 +53,14 @@ public class AddMemoActivity extends AppCompatActivity {
         clearMyPrefs();
         setContentView(R.layout.activity_add_memo);
 
-        photo = (ImageView) findViewById(R.id.imageViewMemo);
+        imageFrame = (LinearLayout)findViewById(R.id.imageFrame);
+        iv_User_Photo = (ImageView) findViewById(R.id.imageViewMemo);
+
         editTextMemo = (EditText) findViewById(R.id.editTextMemo);
         strDate = (TextView) findViewById(R.id.textViewDate);
+
+
+
         strDate.setText(getTimeCutSec());
 
         Intent intent = getIntent();
@@ -62,6 +89,160 @@ public class AddMemoActivity extends AppCompatActivity {
 
 
     }
+
+    public void onClickAddImageClicked(View v){
+        DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doTakePhotoAction();
+            }
+        };
+        DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doTakeAlbumAction();
+            }
+        };
+
+        DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        };
+
+        new AlertDialog.Builder(this)
+                .setTitle("업로드할 이미지 선택")
+                .setPositiveButton("사진촬영", cameraListener)
+                .setNeutralButton("앨범선택", albumListener)
+                .setNegativeButton("취소", cancelListener)
+                .show();
+    }
+
+    public void doTakePhotoAction() // 카메라 촬영 후 이미지 가져오기
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+
+    public void doTakeAlbumAction() // 앨범에서 이미지 가져오기
+    {
+        // 앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK)
+            return;
+
+        switch (requestCode) {
+            case PICK_FROM_ALBUM: {
+                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
+                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+                mImageCaptureUri = data.getData();
+                Log.d("SmartWheel", mImageCaptureUri.getPath().toString());
+            }
+
+            case PICK_FROM_CAMERA: {
+
+
+                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                // CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
+                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_IMAGE); // CROP_FROM_CAMERA case문 이동
+                break;
+            }
+
+            case CROP_FROM_IMAGE: {
+                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
+                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                // 임시 파일을 삭제합니다.
+                if (resultCode != RESULT_OK) {
+                    return;
+                }
+
+                final Bundle extras = data.getExtras();
+
+                // CROP된 이미지를 저장하기 위한 FILE 경로
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/SmartWheel/" + System.currentTimeMillis() + ".jpg";
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
+                    iv_User_Photo.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+
+                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
+                    absolutePath = filePath;
+                    break;
+                }
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+
+            }
+        }
+    }
+
+    private void storeCropImage(Bitmap bitmap, String filePath) {
+        // SmartWheel 폴더를 생성하여 이미지를 저장하는 방식이다.
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel";
+        File directory_SmartWheel = new File(dirPath);
+
+        if(!directory_SmartWheel.exists()) // SmartWheel 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
+            directory_SmartWheel.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            // sendBroadcast를 통해 Crop된 사진을 앨범에 보이도록 갱신한다.
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                    Uri.fromFile(copyFile)));
+
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+    ////////////////////////////// 저장 관련 파트들
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -169,6 +350,7 @@ public class AddMemoActivity extends AppCompatActivity {
 
     }
 
+    /* onsaveinstance ~~는 쓰지않음. onpause / onresume에서 처리
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState); // 반드시 호출해 주세요.
@@ -176,7 +358,7 @@ public class AddMemoActivity extends AppCompatActivity {
 
         // 추가로 자료를 저장하는 코드는 여기에 작성 하세요.
     }
-
+*/
 
     @Override
     protected void onPause() {
