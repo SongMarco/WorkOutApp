56ec2604c10c8257b6c93de794616d949443d0c2
Runner <jamsya@naver.com>
2017-09-08 02:14:47 +0900

    액션바 구현중

--- app/src/main/java/nova/workoutapp22/MainActivity.java
+++ app/src/main/java/nova/workoutapp22/MainActivity.java
@@ -3,6 +3,7 @@ package nova.workoutapp22;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
 
+        setSupportActionBar(myToolbar);
 
         findViewById(R.id.mainToMotiveButton).setOnClickListener(mClickListener);
         findViewById(R.id.mainToWorkoutButton).setOnClickListener(mClickListener);
